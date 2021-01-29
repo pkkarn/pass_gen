2e21c9813496907b6d3bb3e6879abcb340996ad5
pkkarn <pk2psp@gmail.com>
2021-01-29 14:15:09 +0530

    Allowed Host

--- movies/settings.py
+++ movies/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'p5_k&em$s83y2z=y82%s=ye60dv8gved2c3f(a3#jb15w=o3%v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'passgenpk.herokuapp.com'
+]
 
 
 # Application definition
